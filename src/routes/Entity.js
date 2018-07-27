31fb103cd874c68aa5722af48d61fbb93d68e2cc
Karel Hala <khala@redhat.com>
2018-07-27 14:01:06 +0200

    Demo for 27 Jul 2018 to show table and other components

--- src/routes/Entity.js
+++ src/routes/Entity.js
@@ -1,37 +1,120 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
-import { Section } from '@red-hat-insights/insights-frontend-components';
+import { sortBy } from 'lodash';
+import { Section, PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
+import { Pagination } from '@red-hat-insights/insights-frontend-components/components/Pagination';
+import { Table, SortDirection } from '@red-hat-insights/insights-frontend-components/components/Table';
 import { Button } from '@patternfly/react-core';
-import EntityDetails from '../components/inventory/EntityDetails';
-
-import * as actions from '../actions';
-
-const InventoryEntityDetails = connect(({ entityDetails }) => ({ ...entityDetails }))(EntityDetails);
 
 class Entity extends Component {
 
-    constructor (props, ctx) {
-        super(props, ctx);
-        this.id = props.match.params.id;
-        this.store = ctx.store;
+    constructor (props) {
+        super(props);
+        this.state = {
+            cols: ['Animal', '# Legs', '# Eyes', 'Name'],
+            rows: [
+                { cells: ['Dog', 4, 2, 'Bert'] },
+                { cells: ['Cat', 3, 2, 'Tripple leg'] },
+                { cells: ['Octopus', 8, 'None', 'Octodad'] },
+                { cells: ['Camel', 4, 2, 'Camie'] },
+                { cells: ['Dog', 4, 1, 'Oneye'] },
+                { cells: ['Cat', 4, 2, 'Wierdo'] },
+                { cells: ['Elephant', 4, 2, 'Smalie'] },
+                { cells: ['Porcupine', 4, 2, 'Pointy'] },
+                { cells: ['Horse', 4, 2, 'MoonMoon'] },
+                { cells: ['Mouse', 4, 2, 'Jerry'] },
+                { cells: ['Spider', 7, 8, 'Tom'] }
+            ],
+            sortBy: {},
+            itemsPerPage: 10,
+            page: 1
+        };
+        this.onSortChange = this.onSortChange.bind(this);
+        this.toggleCol = this.toggleCol.bind(this);
+        this.limitRows = this.limitRows.bind(this);
+        this.setPage = this.setPage.bind(this);
+        this.setPerPage = this.setPerPage.bind(this);
     }
 
-    componentDidMount () {
-        this.store.dispatch(actions.loadEntity(parseInt(this.id)));
+    toggleCol(_event, key, selected) {
+        let { rows, page, itemsPerPage } = this.state;
+        const firstIndex = page === 1 ? 0 : page * itemsPerPage - itemsPerPage;
+        rows[firstIndex + key].selected = selected;
+        this.setState({
+            ...this.state,
+            rows
+        });
+    }
+
+    onSortChange(_event, key, direction) {
+        const sortedRows = sortBy(this.state.rows, [e => e.cells[key]]);
+        this.setState({
+            ...this.state,
+            rows: SortDirection.up === direction ? sortedRows : sortedRows.reverse(),
+            sortBy: {
+                index: key,
+                direction
+            }
+        });
+    }
+
+    limitRows() {
+        const { page, itemsPerPage } = this.state;
+        const numberOfItems = this.state.rows.length;
+        const lastPage = Math.ceil(numberOfItems / itemsPerPage);
+        const lastIndex = page === lastPage ? numberOfItems : page * itemsPerPage;
+        const firstIndex = page === 1 ? 0 : page * itemsPerPage - itemsPerPage;
+        return this.state.rows.slice(firstIndex, lastIndex);
+    }
+
+    setPage(page) {
+        this.setState({
+            ...this.state,
+            page
+        });
+    }
+
+    setPerPage(amount) {
+        this.setState({
+            ...this.state,
+            itemsPerPage: amount
+        });
     }
 
     render() {
+        const rows = this.limitRows();
         return (
-            <Section type='content'>
-                <InventoryEntityDetails/>
-                <Link to='/inventory'>
-                    <Button variant='primary'>Back</Button>
-                </Link>
-            </Section>
-
+            <React.Fragment>
+                <PageHeader>
+                    <PageHeaderTitle title={`Inventory ID: ${this.props.match.params.id}`}/>
+                </PageHeader>
+                <Section type='content'>
+                    <Table
+                        onItemSelect={this.toggleCol}
+                        hasCheckbox={true}
+                        header={this.state.cols}
+                        sortBy={this.state.sortBy}
+                        rows={rows}
+                        onSort={this.onSortChange}
+                        footer={
+                            <Pagination
+                                numberOfItems={this.state.rows.length}
+                                onPerPageSelect={this.setPerPage}
+                                page={this.state.page}
+                                onSetPage={this.setPage}
+                                itemsPerPage={this.state.itemsPerPage}
+                            />
+                        }
+                    />
+                </Section>
+                <Section type="button-group">
+                    <div className="ins-back-button">
+                        <Link to='/inventory'>
+                            <Button variant='primary'>Back</Button>
+                        </Link></div>
+                </Section>
+            </React.Fragment>
         );
     }
 }
--- src/routes/Inventory.js
+++ src/routes/Inventory.js
@@ -1,37 +1,101 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './inventory.scss';
 
 import { PageHeader, PageHeaderTitle, Section } from '@red-hat-insights/insights-frontend-components';
-import { Button } from '@patternfly/react-core';
-
-import * as actions from '../actions';
-import EntityTable from '../components/inventory/EntityTable';
-
-const InventoryEntityTable = connect(({ entities }) => ({ ...entities }))(EntityTable);
+import { SimpleTableFilter } from '@red-hat-insights/insights-frontend-components/components/SimpleTableFilter';
+import { Dropdown, DropdownItem } from '@red-hat-insights/insights-frontend-components/components/Dropdown';
+import { Card, Level, CardBody, Grid, GridItem, CardHeader, LevelItem } from '@patternfly/react-core';
+import { BellIcon, CubesIcon, MemoryIcon, HddIcon, FolderIcon, ClockIcon, BugIcon } from '@patternfly/react-icons';
 
 class Inventory extends Component {
 
-    constructor (props, ctx) {
-        super(props, ctx);
-        this.loadEntities = () => ctx.store.dispatch(actions.loadEntities());
+    constructor (props) {
+        super(props);
+        this.state = {
+            collapsed: true,
+            cards: [
+                { icon: BellIcon, content: 'one' },
+                { icon: CubesIcon, content: 'two' },
+                { icon: MemoryIcon, content: 'oone' },
+                { icon: HddIcon, content: 'ttwo' },
+                { icon: FolderIcon, content: 'ddd' },
+                { icon: ClockIcon, content: 'mewtwo' },
+                { icon: BugIcon, content: 'tre' }
+            ],
+            span: 2
+        };
+        this.onSpanToggle = this.onSpanToggle.bind(this);
+        this.onSpanSelect = this.onSpanSelect.bind(this);
+        this.onSearch = this.onSearch.bind(this);
+        this.onCardClick = this.onCardClick.bind(this);
     }
 
-    componentDidMount () {
-        this.loadEntities();
+    onSearch(value) {
+        const filteredCards  = this.state.cards.filter(oneCard =>oneCard.content.indexOf(value) !== -1);
+        this.setState({
+            ...this.state,
+            filteredCards
+        });
+    }
+
+    onSpanToggle() {
+        this.setState({
+            ...this.state,
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    onSpanSelect(event) {
+        this.setState({
+            ...this.state,
+            collapsed: !this.state.collapsed,
+            span: parseInt(event.target.getAttribute('value'), 10)
+        });
+    }
+
+    onCardClick(event) {
+        const value = event.currentTarget.getAttribute('value');
+        this.props.history.push(`/inventory/entity/${value}`);
     }
 
     render() {
+        const cards = this.state.filteredCards || this.state.cards;
         return (
             <React.Fragment>
                 <PageHeader>
                     <PageHeaderTitle title='Inventory'/>
                 </PageHeader>
                 <Section type='content'>
-                    <InventoryEntityTable/>
-                    <Button variant='primary' onClick={() => this.loadEntities()}>Refresh</Button>
+                    <Level gutter="sm">
+                        <LevelItem>
+                            <Dropdown
+                                title={`Grid span - ${this.state.span}`}
+                                isCollapsed={this.state.collapsed}
+                                onToggle={this.onSpanToggle}
+                                onSelect={this.onSpanSelect}
+                            >
+                                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((oneSpan, key) => (
+                                    <DropdownItem key={key} value={oneSpan}>{oneSpan}</DropdownItem>
+                                ))}
+                            </Dropdown>
+                        </LevelItem>
+                        <LevelItem>
+                            <SimpleTableFilter onFilterChange={this.onSearch} buttonTitle=""/>
+                        </LevelItem>
+                    </Level>
+                    <Grid gutter="sm">
+                        {cards.map((oneCard, key) => (
+                            <GridItem span={this.state.span} key={key}>
+                                <Card onClick={this.onCardClick} value={key}>
+                                    <CardHeader><oneCard.icon /></CardHeader>
+                                    <CardBody>{oneCard.content}</CardBody>
+                                </Card>
+                            </GridItem>
+                        ))}
+                    </Grid>
+
                 </Section>
             </React.Fragment>
         );
@@ -39,7 +103,8 @@ class Inventory extends Component {
 }
 
 Inventory.contextTypes = {
-    store: PropTypes.object
+    store: PropTypes.object,
+    history: PropTypes.object
 };
 
 export default withRouter(Inventory);
